f33887513826b6ec64149c34afb45fa68fddcd96
lambdaXI <maximeyangemploi@hotmail.com>
2016-10-01 19:08:52 +0200

    -fix header

--- laravel/resources/views/partials/_header.blade.php
+++ laravel/resources/views/partials/_header.blade.php
@@ -23,13 +23,7 @@
             <li><a href="#/">Bande Dessiner</a></li>
           </ul>
         </li>
-      </ul>
-      <form class="navbar-form navbar-left">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Chercher un livre</button>
-      </form>
+      </ul>      
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Membre<span class="caret"></span></a>
